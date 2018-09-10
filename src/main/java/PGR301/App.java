30c31ff208d457e9c0d1909d3373e487f24a24b2
Ola Kveli <olakveli@yahoo.com>
2018-09-10 10:10:47 +0200

    I changed something

--- src/main/java/PGR301/App.java
+++ src/main/java/PGR301/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("kveola13 changed the file");
     }
 }
