3facbbc4e88c23b1cd9761a1ac29ba398662c08a
nimkha <nimak47@gmail.com>
2018-09-10 11:14:58 +0200

    lagt til en sout

--- src/main/java/PGR301/App.java
+++ src/main/java/PGR301/App.java
@@ -4,10 +4,9 @@ package PGR301;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println("Endring for oppgave forelesning 3");
     }
 }
